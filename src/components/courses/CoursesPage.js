695aa8247439dc4daee6eda05f30eed6cfa597f1
adc work <adc9work@outlook.com>
2020-08-05 14:33:55 +0530

    Simple Form Submit Done!

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,41 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  //State Decleration
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = { course: { title: "" } };
+
+  //Binding to the Constructer
+  // this.handleChange = this.handeleChange.bind(this);
+  // }
+
+  state = { course: { title: "" } };
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
+  handleSubmit = (event) => {
+    //Prevent form from posting back
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h3>Courses</h3>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
